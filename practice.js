759148f41c882d62dc089aca9ad161daa40268ff
Farjana Islam <nipa.farzana87@gmailcon>
2020-08-14 18:40:23 +0600

    practice array

---
+++ practice.js
@@ -0,0 +1,27 @@
+const polapainAge = [14, 10, 6, 5, 1, .8];
+
+// to check the value of a position
+const mahveerAge = polapainAge[3];
+
+// to change the value of an item
+polapainAge[2] = 9;
+
+
+// to check position
+let agePosition = polapainAge.indexOf(14);
+
+// to add an element
+polapainAge.push(4);
+
+// to delete an element
+polapainAge.pop();
+
+// to add element at the beginning 
+polapainAge.unshift(13);
+
+// to remove first element
+polapainAge.shift()
+
+// to know the total number of element
+console.log(polapainAge.length);
+console.log(polapainAge);
